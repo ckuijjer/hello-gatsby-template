c19efe1a944dba84091fed931d9c3099c4e4bd6e
Casper Kuijjer <casper@kuijjer.com>
2019-05-03 15:21:45 +0200

    outputs

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,5 @@
 module "landingpage" {
   source = "git::https://cio-innovation-nn-group@dev.azure.com/cio-innovation-nn-group/Catapult/_git/terraform-modules//landingpage"
+
+  # source = "../../terraform-modules/landingpage"
 }
---
+++ infra/outputs.tf
@@ -0,0 +1,15 @@
+output "bucket_id" {
+  value = "${module.landingpage.bucket_id}"
+}
+
+output "bucket_website_endpoint" {
+  value = "${module.landingpage.bucket_website_endpoint}"
+}
+
+output "bucket_website_domain" {
+  value = "${module.landingpage.bucket_website_domain}"
+}
+
+output "bucket_policy_id" {
+  value = "${module.landingpage.bucket_policy_id}"
+}
