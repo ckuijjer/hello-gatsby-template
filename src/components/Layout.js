d9dd1aeafb7a013279725866bc885d4aef589419
Casper Kuijjer <casper@kuijjer.com>
2019-05-29 14:17:50 +0200

    casing change and removal of layout.css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,7 +12,6 @@ import { StaticQuery, graphql } from 'gatsby';
 import { Global, css } from '@emotion/core';
 
 import Header from './header';
-import './layout.css';
 import fonts from '../fonts';
 
 const styles = css`
@@ -32,8 +31,34 @@ const styles = css`
       url(${fonts.NNDagnyTextWebWOFF}) format('woff');
   }
 
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font: 112.5%/1.45em georgia, serif;
+    box-sizing: border-box;
+    overflow-y: scroll;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  @media only screen and (max-width: 480px) {
+    html {
+      font-size: 100%;
+    }
+  }
+
   body {
     font-family: 'NN Dagny';
+    margin: 0;
+    color: hsla(0, 0%, 0%, 0.8);
+    font-weight: normal;
+    word-wrap: break-word;
+    font-kerning: normal;
+    font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   h1,
@@ -42,8 +67,25 @@ const styles = css`
   h4,
   h5,
   h6 {
-    font-family: 'NN Dagny';
+    margin: 0 0 1.45rem;
+    padding: 0;
+    font-weight: bold;
+    text-rendering: optimizeLegibility;
+    font-size: 1.62671rem;
+    line-height: 1.1;
     color: #ea650d;
+  }
+
+  h1 {
+    font-size: 2.25rem;
+  }
+
+  h2 {
+    font-size: 1.62671rem;
+  }
+
+  h3 {
+    font-size: 1.38316rem;
   }
 
   dt {
@@ -70,12 +112,9 @@ const Layout = ({ children }) => (
           style={{
             margin: `0 auto`,
             maxWidth: 960,
-            // padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
           }}
         >
           <main>{children}</main>
-          {/* <footer>© {new Date().getFullYear()}</footer> */}
         </div>
       </>
     )}
